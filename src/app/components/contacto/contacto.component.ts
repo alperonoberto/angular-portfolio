4be6fc80a875a4f953c3c69cb42af35db48f63ce
alperonoberto <alperonoberto@gmail.com>
2023-08-29 01:29:26 +0200

    feat: projects component

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,53 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import emailjs from '@emailjs/browser';
+import {
+  EMAIL_SERVICE_PUBLIC_KEY,
+  EMAIL_SERVICE_TEMPLATE,
+  EMAIL_VALIDATION_API_KEY,
+} from '../../../environment/environment';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.scss']
+  styleUrls: ['./contacto.component.scss'],
 })
 export class ContactoComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-  
+
   contactoForm = new FormGroup({
     nombre: new FormControl('', Validators.required),
     apellidos: new FormControl(''),
     mandatario: new FormControl('', Validators.required),
-    mensaje: new FormControl('', Validators.required)
-  })
+    mensaje: new FormControl('', Validators.required),
+  });
 
   async send(formValue: FormGroup) {
-    emailjs.init('RjgwO398hmsNCPUjK')
-    let response = await emailjs.send("service_64cdrzm", "template_0pb3hmj", {
+    emailjs.init('RjgwO398hmsNCPUjK');
+    let response = await emailjs.send(EMAIL_SERVICE_PUBLIC_KEY, EMAIL_SERVICE_TEMPLATE, {
       to_name: 'Alberto',
       from_name: `${this.contactoForm.value.nombre} ${this.contactoForm.value.apellidos}`,
       from_email: this.contactoForm.value.mandatario,
-      message: this.contactoForm.value.mensaje
-    })
+      message: this.contactoForm.value.mensaje,
+    });
 
     // console.table(formValue.value)
-    alert('🎇 ¡Mensaje enviado! 🎇  Recibirás una respuesta en breve 😄')
-    this.contactoForm.reset()
+    alert('🎇 ¡Mensaje enviado! 🎇  Recibirás una respuesta en breve 😄');
+    this.contactoForm.reset();
   }
 
   
-  options = {method: 'GET'};
-  EMAIL_API_KEY = 'f3119bf5949c4477ae38bc59abdec957'
-
   validateEmail(email: string | null) {
-    let url = `https://emailvalidation.abstractapi.com/v1?api_key=${this.EMAIL_API_KEY}&email=${email}`
-    fetch(url, this.options)
-    .then(response => response.json())
-    .then(response => {
-      if(response.is_valid_format.value) {
-        this.send(this.contactoForm)
-      } else {
-        alert('❌ Algo no ha ido como se esperaba... Revisa tu dirección de correo 🤞')
-      }
-    })
-    .catch(err => console.error(err));
+    const options = { method: 'GET' };
+    let url = `https://emailvalidation.abstractapi.com/v1?api_key=${EMAIL_VALIDATION_API_KEY}&email=${email}`;
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(response => {
+        if (response.is_valid_format.value) {
+          this.send(this.contactoForm);
+        } else {
+          alert(
+            '❌ Algo no ha ido como se esperaba... Revisa tu dirección de correo 🤞'
+          );
+        }
+      })
+      .catch(err => console.error(err));
   }
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,3 @@
+export const EMAIL_VALIDATION_API_KEY = 'f3119bf5949c4477ae38bc59abdec957'
+export const EMAIL_SERVICE_PUBLIC_KEY = 'service_64cdrzm'
+export const EMAIL_SERVICE_TEMPLATE = 'template_0pb3hmj'
