8991edf14b87cb3ea028b341766110510939a2cf
alperonoberto <alperonoberto@gmail.com>
2023-08-28 22:01:02 +0200

    feat: email validation API

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -20,16 +20,34 @@ export class ContactoComponent implements OnInit {
   })
 
   async send(formValue: FormGroup) {
-    // emailjs.init('RjgwO398hmsNCPUjK')
-    // let response = await emailjs.send("service_64cdrzm", "template_0pb3hmj", {
-    //   to_name: 'Alberto',
-    //   from_name: `${this.contactoForm.value.nombre} ${this.contactoForm.value.apellidos}`,
-    //   from_email: this.contactoForm.value.mandatario,
-    //   message: this.contactoForm.value.mensaje
-    // })
+    emailjs.init('RjgwO398hmsNCPUjK')
+    let response = await emailjs.send("service_64cdrzm", "template_0pb3hmj", {
+      to_name: 'Alberto',
+      from_name: `${this.contactoForm.value.nombre} ${this.contactoForm.value.apellidos}`,
+      from_email: this.contactoForm.value.mandatario,
+      message: this.contactoForm.value.mensaje
+    })
 
-    console.table(formValue.value)
-    // alert('🎇¡Mensaje enviado!🎇  Recibirá respuesta una respuesta en breve 😄')
+    // console.table(formValue.value)
+    alert('🎇 ¡Mensaje enviado! 🎇  Recibirás una respuesta en breve 😄')
     this.contactoForm.reset()
+  }
+
+  
+  options = {method: 'GET'};
+  EMAIL_API_KEY = 'f3119bf5949c4477ae38bc59abdec957'
+
+  validateEmail(email: string | null) {
+    let url = `https://emailvalidation.abstractapi.com/v1?api_key=${this.EMAIL_API_KEY}&email=${email}`
+    fetch(url, this.options)
+    .then(response => response.json())
+    .then(response => {
+      if(response.is_valid_format.value) {
+        this.send(this.contactoForm)
+      } else {
+        alert('❌ Algo no ha ido como se esperaba... Revisa tu dirección de correo 🤞')
+      }
+    })
+    .catch(err => console.error(err));
   }
 }
